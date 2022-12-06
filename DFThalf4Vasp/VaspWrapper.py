5694966da5fff2835af0a9dd94652c7045625f01
Joshua Claes <joshua.claes@uantwerpen.be>
2022-12-06 13:11:07 +0100

    Change return 0 for pass

--- DFThalf4Vasp/VaspWrapper.py
+++ DFThalf4Vasp/VaspWrapper.py
@@ -1,14 +1,14 @@
-import os
 """ Some function to interact with vasp on the system"""
 
 class VaspWrapper(object):
     def __init__(self):
-        return 0
-def run_vasp(self, foldervasprun, typevasprun):
-    return 0
+        pass
 
-def calculate_bandgap(self):
-    return 0
+    def run_vasp(self, foldervasprun, typevasprun):
+        pass
 
-def calculate_gap(self,bands,spins,vaspfolder='./',kpoints=0):
-    return 0
+    def calculate_bandgap(self):
+        pass
+
+    def calculate_gap(self, bands, spins, vaspfolder='./', kpoints=0):
+        pass
